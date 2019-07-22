24ca567893b783de918ef5093995c2e533df440e
Edem Agbakpe <edem.agbakpe@gmail.com>
2019-07-22 17:05:55 +0000

    Start

---
+++ wordfrequencycounter.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+import operator
+
+def start(url):
+    word_list = []
+    source_code = requests.get(url).text
+    soup = BeautifulSoup(source_code)
+    for post_text in soup
