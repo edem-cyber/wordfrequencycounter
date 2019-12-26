489ed2576684c67fe618ce23d5029691d938bed6
edem-cyber <51761485+edem-cyber@users.noreply.github.com>
2019-12-26 05:35:37 +0000

    second commit

--- wordfrequencycounter.py
+++ wordfrequencycounter.py
@@ -6,4 +6,16 @@ def start(url):
     word_list = []
     source_code = requests.get(url).text
     soup = BeautifulSoup(source_code)
-    for post_text in soup
+    for post_text in soup('a', class_=''):
+        content = post_text.string
+        words = content.lower().split()
+        for each_word in words:
+            word_list.append(each_word)
+
+
+def clean_up_list(wor_list):
+    clean_word_list = []
+    for word in word_list:
+        symbols = "`~!@#$%^&*()_+{}|:\"<>?-=[];',./£\!"
+        for i in range(0, len(symbols)):
+            word = word.replace(symbols[i], '')
